e236386162a2a419d991ed1a4520ab2ea8f6685a
chandiran-dmc <dmohanachandiran@gmail.com>
2018-10-20 23:45:00 -0400

    First commit of sample android safety driving app

--- App.js
+++ App.js
@@ -1,41 +1,109 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
-import { ButtonGroup } from 'react-native-elements'; // 0.17.0
-import '@expo/vector-icons'; // 5.2.0
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import { Button } from 'react-native';
+import {
+  createStackNavigator,
+} from 'react-navigation';
 
-export default class App extends Component {
-  state = {
-    index: 0,
-  };
+import { View } from 'react-native';
 
-  updateIndex = index => {
-    this.setState({ index });
-  };
+var newLatitude = null;
+var newLongitude = null;
 
+function getlocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getCoordinates);
+  } else { 
+      alert("4");
+  }
+}
+
+function getCoordinates(position) {
+  currentLatitude = position.coords.latitude;
+  currentLongitude = position.coords.longitude; 
+
+  // now latitude and longitude are available
+  newLatitude=currentLatitude;
+  newLongitude=currentLongitude;
+}
+
+
+const storeUserInfo = async () => {
+  try {
+    this._interval = setInterval(() => {
+      bob.callFunction("updatePos", [object1]).then(echoedResult => {
+        console.log(`Success!`);
+        console.log(echoedResult);   
+      })
+      // getlocation();
+      // console.log('latitude: '+newLatitude+'  longitude: '+newLongitude);
+    }
+    
+    
+    , 2000);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+const { Stitch, AnonymousCredential } = require('mongodb-stitch-react-native-sdk');
+var bob = null;
+Stitch.initializeDefaultAppClient('csdh-hcbvo').then(client => {
+    // use the client.
+    bob = client;
+    console.log(client);
+    console.log("logging in anonymously");
+client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+  console.log(`logged in anonymously as user ${user.id}`)
+});
+});
+
+
+
+
+
+var object1 = {
+  loc : { type: "PointBitch", coordinates: [ -73.95, 40.75 ] },
+  name: "FuckingHereAsshole",
+  category : "WorkBitch"
+}
+
+
+class LogInScreen extends React.Component {
+    
   render() {
+
+    const { navigate } = this.props.navigation;
     return (
-      <View style={{ position: 'absolute', bottom: 50, width: "100%" }}>
-        <ButtonGroup
-          selectedBackgroundColor=""
-          onPress={this.updateIndex}
-          selectedIndex={this.state.index}
-          buttons={['Data Points', 'Call Help', 'Heatmap']}
-          containerStyle={{ height: 40, width: "auto" }}
-        />
+      <View>
+      <Button
+        title="Go to LogIn"
+        onPress={() =>
+          storeUserInfo()
+
+        }
+      />
+      
       </View>
+      
+      
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: '',
-    justifyContent: '',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ec',
-  },
-});
+export default class App extends React.Component {
+  render() {
+    return <StackNavigation />;
+  }
+}
+
+
+const StackNavigation = createStackNavigator({
+  Home: { screen: LogInScreen },
+  
+},
+{
+  initialRouteName: 'Home',
+}
+);
+
