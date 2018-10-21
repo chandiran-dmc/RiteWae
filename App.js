ae6b15c189dfd90c05a0746841b30ecd4bdbd349
Ryan Luu <rluu@purdue.edu>
2018-10-20 23:46:29 -0400

    Map and toggle speech command button

--- App.js
+++ App.js
@@ -1,41 +1,349 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Constants } from 'expo';
-import { ButtonGroup } from 'react-native-elements'; // 0.17.0
-import '@expo/vector-icons'; // 5.2.0
-import { Button } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import Voice from 'react-native-voice';
+import { Button } from 'native-base';
 
-export default class App extends Component {
-  state = {
-    index: 0,
-  };
+export default class App extends React.Component {
 
-  updateIndex = index => {
-    this.setState({ index });
-  };
+  constructor() {
+    super();
+    this.active = false;
+    this.speech = "";
+    Voice._onSpeechStart = () => alert("_speechStart");
+    Voice.onSpeechStart = () => alert("speechStart");
+    Voice.onSpeechResults = this.onSpeechResultsHandler.bind(this);
+  }
+
+  onSpeechResultsHandler(event) {
+    this.speech = event;
+    alert(event);
+  }
 
   render() {
     return (
-      <View style={{ position: 'absolute', bottom: 50, width: "100%" }}>
-        <ButtonGroup
-          selectedBackgroundColor=""
-          onPress={this.updateIndex}
-          selectedIndex={this.state.index}
-          buttons={['Data Points', 'Call Help', 'Heatmap']}
-          containerStyle={{ height: 40, width: "auto" }}
-        />
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: 59.3293235,
+            longitude: 18.06858,
+            latitudeDelta: 0.1,
+            longitudeDelta: 0.1
+          }}
+          customMapStyle={[
+            {
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#232b32"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#8ec3b9"
+                }
+              ]
+            },
+            {
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#1a3646"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.country",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#4b6878"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.land_parcel",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#64779e"
+                }
+              ]
+            },
+            {
+              "featureType": "administrative.province",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#4b6878"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.man_made",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#334e87"
+                }
+              ]
+            },
+            {
+              "featureType": "landscape.natural",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#012f43"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#22355b"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#6f9ba5"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#1d2c4d"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#214045"
+                }
+              ]
+            },
+            {
+              "featureType": "poi.park",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#3C7680"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#98a5be"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#1d2c4d"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#334b79"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#2b58ac"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#b0d5ce"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#023e58"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#3d4045"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#98a5be"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "elementType": "labels.text.stroke",
+              "stylers": [
+                {
+                  "color": "#1d2c4d"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.line",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#283d6a"
+                }
+              ]
+            },
+            {
+              "featureType": "transit.station",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#3a4762"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "color": "#0e1626"
+                }
+              ]
+            },
+            {
+              "featureType": "water",
+              "elementType": "labels.text.fill",
+              "stylers": [
+                {
+                  "color": "#4e6d70"
+                }
+              ]
+            }
+          ]}
+        >
+        </MapView>
+        <Button style={this.fabStyle()} onPress={this.toggleActive.bind(this)}>
+        <Text style={this.btnTextStyle()}>
+        {this.active ? "Listening..." : "Speak"}
+        </Text>
+        </Button>
+        <Text style={styles.speech}>Said: {this.speech}</Text>
       </View>
     );
   }
+
+  fabStyle() {
+    var fabContainer = {
+      borderWidth: 1,
+      height: 120,
+      width: 120,
+      borderRadius: 60,
+      alignItems: "center",
+      justifyContent: "center",
+      position: "absolute",
+      bottom: 20,
+      left: 20,
+      shadowColor: "#000",
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      shadowOffset: {
+          height: 1,
+          width: 0
+      }
+    }
+    if (this.active) {
+      fabContainer.backgroundColor = "#2b58ac";
+    } else {
+      fabContainer.backgroundColor = "rgba(255,255,255,0.5)";
+    }
+    return fabContainer;
+  }
+
+  btnTextStyle() {
+    return {fontSize: 21, color: (this.active) ? "#FFF" : "#000"};
+  }
+
+  toggleActive() {
+    alert(JSON.stringify(Voice.isAvailable()));
+    if (!this.active) {
+      alert("VOICE START" + JSON.stringify(Voice.start('en-US')));
+      this.active = true;
+    } else {
+      Voice.stop();
+      this.active = false;
+    }
+    this.forceUpdate();
+  }
 }
 
-const styles = StyleSheet.create({
+
+let styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: '',
-    justifyContent: '',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ec',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
+  },
+  speech: {
+    position: "absolute",
+    top: 20,
+    left: 20,
+    color: "#FFF",
+    fontSize: 12
+  }
 });
